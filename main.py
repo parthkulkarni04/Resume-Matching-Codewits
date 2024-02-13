9f32d224eccc7699bc5950f0766b39f64096ec5d
Parth Kulkarni <114288341+parthkulkarni04@users.noreply.github.com>
2024-02-13 13:42:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,90 +77,92 @@ def v_spacer(height, sb=False) -> None:
 # Streamlit Frontend
 st.title("Resume Matching Tool📃📃")
 
-# Sidebar - File Upload for Job Descriptions
-st.sidebar.write("## Upload Job Description PDF")
-job_descriptions_file = st.sidebar.file_uploader(
-    "Upload Job Description PDF", type=["pdf"])
-
 # Sidebar - File Upload for Resumes
 st.sidebar.write("## Upload Resumes PDF")
 resumes_files = st.sidebar.file_uploader(
     "Upload Resumes PDF", type=["pdf"], accept_multiple_files=True)
 
-# Sidebar - Sorting Options
-sort_options = ['Similarity Score', 'CGPA', 'Total Score']
-selected_sort_option = st.sidebar.selectbox("Sort results by", sort_options)
+if resumes_files:
+    # Sidebar - File Upload for Job Descriptions
+    st.sidebar.write("## Upload Job Description PDF")
+    job_descriptions_file = st.sidebar.file_uploader(
+        "Upload Job Description PDF", type=["pdf"])
 
-# Backend Processing
-if job_descriptions_file is not None and resumes_files is not None:
-    job_description_text = extract_text_from_pdf(job_descriptions_file)
-    resumes_texts = [extract_text_from_pdf(resume_file) for resume_file in resumes_files]
+    if job_descriptions_file:
+        # Sidebar - Sorting Options
+        sort_options = ['Similarity Score', 'CGPA', 'Total Score']
+        selected_sort_option = st.sidebar.selectbox("Sort results by", sort_options)
 
-    # Calculate skills for all resumes
-    all_resumes_skills = [extract_skills(resume_text) for resume_text in resumes_texts]
+        # Backend Processing
+        job_description_text = extract_text_from_pdf(job_descriptions_file)
+        resumes_texts = [extract_text_from_pdf(resume_file) for resume_file in resumes_files]
 
-    tagged_resumes = [TaggedDocument(words=preprocess_text(text), tags=[str(i)]) for i, text in enumerate(resumes_texts)]
-    model_resumes = train_doc2vec_model(tagged_resumes)
+        # Calculate skills for all resumes
+        all_resumes_skills = [extract_skills(resume_text) for resume_text in resumes_texts]
 
-    results_data = {'Resume': [], 'Similarity Score': [], 'CGPA': [], 'Total Score': [], 'Email': [], 'Contact': []}
+        tagged_resumes = [TaggedDocument(words=preprocess_text(text), tags=[str(i)]) for i, text in enumerate(resumes_texts)]
+        model_resumes = train_doc2vec_model(tagged_resumes)
 
-    for i, resume_text in enumerate(resumes_texts):
-        similarity_score = calculate_similarity(model_resumes, resume_text, job_description_text)
-        cgpa_values = extract_cgpa(resume_text)
-        cgpa = ', '.join(map(str, cgpa_values)) if cgpa_values else '0'
-        results_data['Resume'].append(f"Resume {i+1}")
-        smScore = similarity_score * 100
-        total_score = smScore + sum(cgpa_values)
-        results_data['Similarity Score'].append(smScore)
-        results_data['CGPA'].append(cgpa)
-        results_data['Total Score'].append(total_score)
+        results_data = {'Resume': [], 'Similarity Score': [], 'CGPA': [], 'Total Score': [], 'Email': [], 'Contact': []}
 
-        emails = ', '.join(re.findall(email_pattern, resume_text))
-        contacts = ', '.join(re.findall(phone_pattern, resume_text))
-        results_data['Email'].append(emails)
-        results_data['Contact'].append(contacts)
+        for i, resume_text in enumerate(resumes_texts):
+            similarity_score = calculate_similarity(model_resumes, resume_text, job_description_text)
+            cgpa_values = extract_cgpa(resume_text)
+            cgpa = ', '.join(map(str, cgpa_values)) if cgpa_values else '0'
+            results_data['Resume'].append(f"Resume {i+1}")
+            smScore = similarity_score * 100
+            total_score = smScore + sum(cgpa_values)
+            results_data['Similarity Score'].append(smScore)
+            results_data['CGPA'].append(cgpa)
+            results_data['Total Score'].append(total_score)
 
-    # Create a DataFrame
-    results_df = pd.DataFrame(results_data)
+            emails = ', '.join(re.findall(email_pattern, resume_text))
+            contacts = ', '.join(re.findall(phone_pattern, resume_text))
+            results_data['Email'].append(emails)
+            results_data['Contact'].append(contacts)
 
-    # Sort the DataFrame based on user-selected option
-    if selected_sort_option == 'Similarity Score':
-        results_df = results_df.sort_values(by='Similarity Score', ascending=False)
-    elif selected_sort_option == 'CGPA':
-        results_df = results_df.sort_values(by='CGPA', ascending=False)
+        # Create a DataFrame
+        results_df = pd.DataFrame(results_data)
+
+        # Sort the DataFrame based on user-selected option
+        if selected_sort_option == 'Similarity Score':
+            results_df = results_df.sort_values(by='Similarity Score', ascending=False)
+        elif selected_sort_option == 'CGPA':
+            results_df = results_df.sort_values(by='CGPA', ascending=False)
+        else:
+            results_df = results_df.sort_values(by='Total Score', ascending=False)
+
+        # Display the sorted results table
+        st.subheader(f"Results Table (Sorted by {selected_sort_option}):")
+        st.table(results_df)
+
+        # Create a DataFrame for skills distribution
+        skills_distribution_data = {'Resume': [], 'Skill': [], 'Frequency': []}
+        for i, resume_skills in enumerate(all_resumes_skills):
+            for skill in set(resume_skills):
+                skills_distribution_data['Resume'].append(f"Resume {i+1}")
+                skills_distribution_data['Skill'].append(skill)
+                skills_distribution_data['Frequency'].append(resume_skills.count(skill))
+
+        skills_distribution_df = pd.DataFrame(skills_distribution_data)
+
+        # Pivot the DataFrame for heatmap
+        skills_heatmap_df = skills_distribution_df.pivot(index='Resume', columns='Skill', values='Frequency').fillna(0)
+
+        # Normalize the values for better visualization
+        skills_heatmap_df_normalized = skills_heatmap_df.div(skills_heatmap_df.sum(axis=1), axis=0)
+
+        # Plot the heatmap
+        fig, ax = plt.subplots(figsize=(12, 8))
+
+        sns.heatmap(skills_heatmap_df_normalized, cmap='YlGnBu', annot=True, fmt=".2f", ax=ax)
+        ax.set_title('Heatmap for Skills Distribution')
+        ax.set_xlabel('Resume')
+        ax.set_ylabel('Skill')
+
+        # Display the Matplotlib figure using st.pyplot()
+        st.pyplot(fig)
     else:
-        results_df = results_df.sort_values(by='Total Score', ascending=False)
-
-    # Display the sorted results table
-    st.subheader(f"Results Table (Sorted by {selected_sort_option}):")
-    st.table(results_df)
-
-    # Create a DataFrame for skills distribution
-    skills_distribution_data = {'Resume': [], 'Skill': [], 'Frequency': []}
-    for i, resume_skills in enumerate(all_resumes_skills):
-        for skill in set(resume_skills):
-            skills_distribution_data['Resume'].append(f"Resume {i+1}")
-            skills_distribution_data['Skill'].append(skill)
-            skills_distribution_data['Frequency'].append(resume_skills.count(skill))
-
-    skills_distribution_df = pd.DataFrame(skills_distribution_data)
-
-    # Pivot the DataFrame for heatmap
-    skills_heatmap_df = skills_distribution_df.pivot(index='Resume', columns='Skill', values='Frequency').fillna(0)
-
-    # Normalize the values for better visualization
-    skills_heatmap_df_normalized = skills_heatmap_df.div(skills_heatmap_df.sum(axis=1), axis=0)
-
-    # Plot the heatmap
-    fig, ax = plt.subplots(figsize=(12, 8))
-
-    sns.heatmap(skills_heatmap_df_normalized, cmap='YlGnBu', annot=True, fmt=".2f", ax=ax)
-    ax.set_title('Heatmap for Skills Distribution')
-    ax.set_xlabel('Resume')
-    ax.set_ylabel('Skill')
-
-    # Display the Matplotlib figure using st.pyplot()
-    st.pyplot(fig)
+        st.write("Please upload the Job Description PDF to proceed.")
 else:
-    st.write("Upload the Job Description and Resumes PDF files to see the results.")
-
+    st.write("Please upload Resumes PDF to proceed.")
